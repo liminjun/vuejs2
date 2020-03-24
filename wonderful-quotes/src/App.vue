1de0aa5fba4369a893af5849a1572f961f61799d
liminjun <liminjun@cmss.chinamobile.com>
2020-03-24 21:03:58 +0800

    feat:input with forms.

---
+++ wonderful-quotes/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        
+    </div>
+</template>
+
+<script>
+    import User from './components/User.vue';
+
+    export default {
+        data: function(){
+            return {
+                quotes: ['Just q Quote to see somthing.'],
+                maxQuotes: 10
+            }
+        },
+        components: {
+            appUser: User
+        }
+    }
+</script>
+
+<style>
+    div.component {
+        border: 1px solid black;
+        padding: 30px;
+    }
+</style>
---
+++ wonderful-quotes/src/components/Quote.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="panel panel-default">
+            <div class="panel-body quote">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default{
+        props:['quotes']
+    }
+</script>
+<style>
+    .panel-body{
+        font-family: 'Arial';
+        font-size: 24px;
+        color: #ee6633;
+    }
+    .quote{
+        background-color: #ffe2e2;
+    }
+</style>
