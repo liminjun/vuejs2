2f8dd20cb134fd5df66e1d4de9d4fd9190e6cd7b
liminjun <v-leeli@hotmail.com>
2020-05-24 21:12:10 +0800

    feat:add filter.

--- vuejs-complete/src/App.vue
+++ vuejs-complete/src/App.vue
@@ -1,44 +1,28 @@
 <template>
   <div class="container">
-    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
-    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
     <div class="row">
-      <div class="col-sm-12 text-center">
-        <div class="alert alert-info">点击卡片可以删除目标。</div>
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters and Mixins</h1>
+        <p>{{text | toUppercase}}</p>
+        <p>{{'HELLO WORLD' | toLowercase}}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import QuoteGrid from "./components/QuoteGrid";
-import NewQuote from "./components/NewQuote";
-import Header from "./components/Header";
-
 export default {
-  data: function() {
+  data() {
     return {
-      quotes: ["示例目标"],
-      maxQuotes: 10
-    };
-  },
-  methods: {
-    newQuote(quote) {
-      if (this.quotes.length < this.maxQuotes) {
-        this.quotes.push(quote);
-      }
-    },
-    deleteQuote(index) {
-      this.quotes.splice(index, 1);
+      text: 'hello world'
     }
   },
-  components: {
-    appQuoteGrid: QuoteGrid,
-    appNewQuote: NewQuote,
-    appHeader: Header
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase();
+    }
   }
-};
+}
 </script>
 
 <style>
--- vuejs-complete/src/main.js
+++ vuejs-complete/src/main.js
@@ -1,15 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-export const eventBus = new Vue({
-
-  methods: {
-    changeAge(age) {
-      this.$emit('ageWasEdited', age);
-    }
-  }
-});
-Vue.config.productionTip = false
+Vue.filter('to-lowercase')
 
 new Vue({
   render: h => h(App),
