ed5be3ea1f37b3a7168eddf2df6756eb42239a6b
liminjun <v-leeli@hotmail.com>
2020-05-31 21:34:45 +0800

    feat:add animation and transition demo

---
+++ animations-transitions/src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+        <hr />
+        <button class="btn btn-primary" @click="show=!show">Show Alert</button>
+        <br />
+        <br />
+        <transition name="fade">
+          <div class="alert alert-info" v-if="show">This is some info.</div>
+        </transition>
+        <transition name="slide" type="animation">
+          <div class="alert alert-info" v-if="show">This is some info.</div>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import List from "./List.vue";
+import { fruitMixin } from "./fruitMixin";
+export default {
+  mixins: [fruitMixin],
+  data() {
+    return {
+      show: false
+    };
+  }
+};
+</script>
+
+<style>
+.fade-enter{
+  opacity: 0;
+}
+.fade-enter-active{
+  transition: opacity 1s;
+}
+.fade-leave {
+  /* opacity: 1; */
+}
+.fade-leave-active {
+   transition: opacity 1s;
+   opacity: 0;
+}
+.slide-enter{
+  opacity: 0;
+  /* animation: slide-in 1s ease-out forwards; */
+}
+.slide-enter-active{
+  animation: slide-in 1s ease-out forwards;
+  transition: opacity 0.5s;
+}
+.slide-leave {
+  /* opacity: 1; */
+}
+.slide-leave-active {
+   animation: slide-out 1s ease-out forwards;
+   transition: opacity 3s;
+   opacity: 0;
+}
+@keyframes slide-in {
+  from {
+    transition: translateY(20px);
+  }
+  to {
+    transition: translateY(0);
+  }
+}
+@keyframes slide-out {
+  form {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(20px);
+  }
+}
+</style>
---
+++ animations-transitions/src/List.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Filters and Mixins</h1>
+
+    <input type="text" v-model="filterText" />
+    <ul>
+      <li v-for="fruit in filteredFruits" :key="fruit">{{fruit}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { fruitMixin}  from './fruitMixin';
+
+export default {
+    mixins: [fruitMixin],
+    created() {
+        console.log("Inside List Created Hook");
+    }
+};
+</script>
+
+<style>
+</style>
